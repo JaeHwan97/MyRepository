8525a278ba92bc11ae8e98f163cccb19d8bc646e
82105 <82105@DESKTOP-863RC7M>
2022-06-22 19:53:40 +0900

    add a new line

--- MyGit_Java/src/MyClass.java
+++ MyGit_Java/src/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD");
+		System.out.println("HELLO~~~ ");
 	}
 
 }
