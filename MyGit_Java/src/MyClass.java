9fba52114b9205c8422db539435b82b03ef68ba4
82105 <82105@DESKTOP-863RC7M>
2022-06-22 20:21:18 +0900

    FIRST COMMIT IN DEV branch

--- MyGit_Java/src/MyClass.java
+++ MyGit_Java/src/MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD");
 		System.out.println("HELLO~~~ ");
+		A myHello = new A();
+		myHello.sayHello1();
 	}
 
 }
